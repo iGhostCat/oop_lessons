b582e99a8f79d67974f621d66b707bbb47b8f588
iGhostCat <alfa-1002@mail.ru>
2025-06-22 12:29:42 +0300

    classes updated

--- src/classes.py
+++ src/classes.py
@@ -5,6 +5,16 @@ class Product:
         self.price = price
         self.quantity = quantity
 
+    @classmethod
+    def new_product(cls, prod_dict):
+        '''Принимает данные нового продукта в словаре'''
+        return cls(str(prod_dict["name"]),
+                   str(prod_dict["description"]),
+                   float(prod_dict["price"]),
+                   int(prod_dict["quantity"]))
+
+    def get_product(self):
+        return f"Product({self.name}, {self.price}, {self.quantity})"
 
 class Category:
     _total_categories = 0
@@ -15,7 +25,7 @@ class Category:
     def __init__(self, name: str, description: str, products: list):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products.copy()
 
         if name not in self._unique_names:
             self._unique_names.add(name)
@@ -23,3 +33,33 @@ class Category:
             Category.category_count = Category._total_categories
 
         self.product_count = len(products)  # Атрибут экземпляра
+
+    @property
+    def products(self):
+        """Геттер для получения списка продуктов"""
+        return self.__products.copy()  # Возвращаем копию для защиты от изменений
+
+    def add_product(self, product):
+        """Метод для добавления одного продукта"""
+        self.__products.append(product)
+        self.product_count = len(self.__products)
+
+    def add_cls_product(self, prod_instance):
+        self.add_product(prod_instance.name)
+
+products_data = [
+    {
+        "name": "Samsung Galaxy S23",
+        "description": "Флагманский смартфон Samsung",
+        "price": 79999.0,
+        "quantity": 15
+    },
+    {
+        "name": "iPhone 15",
+        "description": "Флагманский смартфон Apple",
+        "price": 89999.0,
+        "quantity": 10
+    }
+]
+prod_1 = Product.new_product(products_data[0])
+print(prod_1.get_product())
