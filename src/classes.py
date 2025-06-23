da20cbf63609bde8d1c787ce5623838eb1c9b2b2
krol.lm <krol.lm@talantiuspeh.ru>
2025-06-23 15:14:06 +0300

    hw 15.2 started

--- src/classes.py
+++ src/classes.py
@@ -5,6 +5,9 @@ class Product:
         self.__price = price
         self.quantity = quantity
 
+    def __str__(self):
+        return f'{self.name}, {self.__price} руб. Остаток: {self.quantity} шт. '
+
     @property
     def price(self):
         return self.__price
